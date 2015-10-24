37ce9682a516f992e3be4d51a658e9133ccb5563
Randy Ting <randy@randyting.com>
2015-10-23 20:30:19 -0700

    Started implementing AMRMessagesViewController.  Still some errors.

--- ArmoireApp/ArmoireApp/AMRMessagesViewController.swift
+++ ArmoireApp/ArmoireApp/AMRMessagesViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import LayerKit
 
-class AMRMessagesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class AMRMessagesViewController: ATLConversationListViewController {
 
-  var messages: NSDictionary?
-  @IBOutlet weak var messagesTable: UITableView!
+  var stylist: AMRUser?
+  var client: AMRUser?
+  
+  // MARK: - Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
     self.title = "Messages"
@@ -23,7 +26,9 @@ class AMRMessagesViewController: UIViewController, UITableViewDataSource, UITabl
     var leftNavBarButton = UIBarButtonItem(customView: settings)
     self.navigationItem.leftBarButtonItem = leftNavBarButton
     
-    // Do any additional setup after loading the view.
+    self.dataSource = self
+    self.delegate = self
+
   }
 
   override func didReceiveMemoryWarning() {
@@ -35,28 +40,61 @@ class AMRMessagesViewController: UIViewController, UITableViewDataSource, UITabl
     let settingsVC = AMRSettingsViewController()
     navigationController?.pushViewController(settingsVC, animated: true)
   }
-  
-  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-  //need to deque from table; planning to switch over to different view than table so wait
-    let cell = messagesTable.dequeueReusableCellWithIdentifier("MessageCell", forIndexPath: indexPath) as! AMRMessageTableViewCell
-    return cell
-  }
 
-  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    if let messageList = self.messages {
-      return messageList.count
-    } else {
-      return 0
+
+
+}
+
+// MARK: - ATLConversationListViewController Delegate
+extension AMRMessagesViewController: ATLConversationListViewControllerDelegate {
+  func conversationListViewController(conversationListViewController: ATLConversationListViewController, didSelectConversation conversation:LYRConversation) {
+//    self.presentControllerWithConversation(conversation)
+  }
+  
+  func conversationListViewController(conversationListViewController: ATLConversationListViewController, didDeleteConversation conversation: LYRConversation, deletionMode: LYRDeletionMode) {
+    print("Conversation deleted")
+  }
+  
+  func conversationListViewController(conversationListViewController: ATLConversationListViewController, didFailDeletingConversation conversation: LYRConversation, deletionMode: LYRDeletionMode, error: NSError?) {
+    print("Failed to delete conversation with error: \(error)")
+  }
+  
+  func conversationListViewController(conversationListViewController: ATLConversationListViewController, didSearchForText searchText: String, completion: ((Set<NSObject>!) -> Void)?) {
+    AMRUserManager.sharedManager.queryForUserWithName(searchText) { (participants: NSArray?, error: NSError?) in
+      if error == nil {
+        if let callback = completion {
+          callback(NSSet(array: participants as! [AnyObject]) as Set<NSObject>)
+        }
+      } else {
+        if let callback = completion {
+          callback(nil)
+        }
+        print("Error searching for Users by name: \(error)")
+      }
     }
   }
-  /*
-  // MARK: - Navigation
-
-  // In a storyboard-based application, you will often want to do a little preparation before navigation
-  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-      // Get the new view controller using segue.destinationViewController.
-      // Pass the selected object to the new view controller.
+  
+  func conversationListViewController(conversationListViewController: ATLConversationListViewController!, avatarItemForConversation conversation: LYRConversation!) -> ATLAvatarItem! {
+    let userID: String = conversation.lastMessage.sender.userID
+    if userID == AMRUser.currentUser()!.objectId {
+      return AMRUser.currentUser()
+    }
+    let user: AMRUser? = AMRUserManager.sharedManager.cachedUserForUserID(userID)
+    if user == nil {
+      AMRUserManager.sharedManager.queryAndCacheUsersWithIDs([userID], completion: { (participants, error) in
+        if participants != nil && error == nil {
+          self.reloadCellForConversation(conversation)
+        } else {
+          print("Error querying for users: \(error)")
+        }
+      })
+    }
+    return user;
   }
-  */
 
+}
+
+// MARK: - ATLConversationListViewController Datasource
+extension AMRMessagesViewController: ATLConversationListViewControllerDataSource {
+  
 }
---
+++ ArmoireApp/ArmoireApp/Models/AMRUserManager.swift
@@ -0,0 +1,99 @@
+//
+//  AMRUserManager.swift
+//  ArmoireApp
+//
+//  Created by Randy Ting on 10/23/15.
+//  Copyright © 2015 Armoire. All rights reserved.
+//
+
+import Foundation
+
+class AMRUserManager {
+  static let sharedManager = AMRUserManager()
+  var userCache: NSCache = NSCache()
+  
+  // MARK Query Methods
+  func queryForUserWithName(searchText: String, completion: ((NSArray?, NSError?) -> Void)) {
+    let query: PFQuery! = AMRUser.query()
+    query.whereKey("objectId", notEqualTo: AMRUser.currentUser()!.objectId!)
+    
+    query.findObjectsInBackgroundWithBlock { objects, error in
+      var contacts = [AMRUser]()
+      if (error == nil) {
+        for user: AMRUser in (objects as! [AMRUser]) {
+          if user.fullName.rangeOfString(searchText, options: NSStringCompareOptions.CaseInsensitiveSearch) != nil {
+            contacts.append(user)
+          }
+        }
+      }
+      completion(contacts, error)
+    }
+  }
+  
+  func queryForAllUsersWithCompletion(completion: ((NSArray?, NSError?) -> Void)?) {
+    let query: PFQuery! = AMRUser.query()
+    query.whereKey("objectId", notEqualTo: AMRUser.currentUser()!.objectId!)
+    query.findObjectsInBackgroundWithBlock { objects, error in
+      if let callback = completion {
+        callback(objects, error)
+      }
+    }
+  }
+  
+  func queryAndCacheUsersWithIDs(userIDs: [String], completion: ((NSArray?, NSError?) -> Void)?) {
+    let query: PFQuery! = AMRUser.query()
+    query.whereKey("objectId", containedIn: userIDs)
+    query.findObjectsInBackgroundWithBlock { objects, error in
+      if (error == nil) {
+        for user: AMRUser in (objects as! [AMRUser]) {
+          self.cacheUserIfNeeded(user)
+        }
+      }
+      if let callback = completion {
+        callback(objects, error)
+      }
+    }
+  }
+  
+  func cachedUserForUserID(userID: NSString) -> AMRUser? {
+    if self.userCache.objectForKey(userID) != nil {
+      return self.userCache.objectForKey(userID) as! AMRUser?
+    }
+    return nil
+  }
+  
+  func cacheUserIfNeeded(user: AMRUser) {
+    if self.userCache.objectForKey(user.objectId!) == nil {
+      self.userCache.setObject(user, forKey: user.objectId!)
+    }
+  }
+  
+  func unCachedUserIDsFromParticipants(participants: NSArray) -> NSArray {
+    var array = [String]()
+    for userID: String in (participants as! [String]) {
+      if (userID == AMRUser.currentUser()!.objectId!) {
+        continue
+      }
+      if self.userCache.objectForKey(userID) == nil {
+        array.append(userID)
+      }
+    }
+    
+    return NSArray(array: array)
+  }
+  
+  func resolvedNamesFromParticipants(participants: NSArray) -> NSArray {
+    var array = [String]()
+    for userID: String in (participants as! [String]) {
+      if (userID == AMRUser.currentUser()!.objectId!) {
+        continue
+      }
+      if self.userCache.objectForKey(userID) != nil {
+        let user: AMRUser = self.userCache.objectForKey(userID) as! AMRUser
+        array.append(user.firstName)
+      }
+    }
+    return NSArray(array: array)
+  }
+
+}
