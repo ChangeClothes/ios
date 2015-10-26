7f34a8a0f04476036676c0d79228996921d1088e
Morgan Wildermuth <morgan.wildermuth@gmail.com>
2015-10-25 19:25:16 -0700

    naive email implementation; brings up email rather than sending in background

--- ArmoireApp/ArmoireApp/ViewControllers/AMRAddClientViewController.swift
+++ ArmoireApp/ArmoireApp/ViewControllers/AMRAddClientViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import MessageUI
 
-class AMRAddClientViewController: UIViewController, AMRViewControllerProtocol {
+class AMRAddClientViewController: UIViewController, AMRViewControllerProtocol, MFMailComposeViewControllerDelegate {
 
   var client: AMRUser?
   var stylist: AMRUser?
@@ -32,6 +33,36 @@ class AMRAddClientViewController: UIViewController, AMRViewControllerProtocol {
   }
 
   @IBAction func onInviteClientTap(sender: UIButton) {
+    let mailComposeViewController = configuredMailComposeViewController()
+    if MFMailComposeViewController.canSendMail() {
+      self.presentViewController(mailComposeViewController, animated: true, completion: nil)
+    } else {
+      self.showSendMailErrorAlert()
+    }
+  }
+
+  func configuredMailComposeViewController() -> MFMailComposeViewController {
+    let mailComposerVC = MFMailComposeViewController()
+    mailComposerVC.mailComposeDelegate = self
+
+    var client_email = clientEmailTextField.text!
+    mailComposerVC.setToRecipients([client_email])
+    mailComposerVC.setSubject("Your Personal Stylist Invites You To Armoire")
+    mailComposerVC.setMessageBody("You have been invited to use Armoire, a tool to keep track of your communications, meetings, and conversations with your personal stylist. Download Armoire from here and use the password 'testpassword' to sign in. You'll be prompted to change the password before getting access to your Stylist Created profile for security purposes. Welcome!", isHTML: false)
+
+    return mailComposerVC
+  }
+
+  func showSendMailErrorAlert() {
+    let sendMailErrorAlert = UIAlertView(title: "Could Not Send Email", message: "Your device could not send e-mail.  Please check e-mail configuration and try again.", delegate: self, cancelButtonTitle: "OK")
+    sendMailErrorAlert.show()
+  }
+
+  // MARK: MFMailComposeViewControllerDelegate
+
+  func mailComposeController(controller: MFMailComposeViewController!, didFinishWithResult result: MFMailComposeResult, error: NSError!) {
+    controller.dismissViewControllerAnimated(true, completion: nil)
+
   }
 
   internal func setVcData(stylist: AMRUser?, client: AMRUser?) {
