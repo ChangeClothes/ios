031be39af8ba0d64224f710f052b373fb0e407c2
Mathew Kellogg <mathew.kellogg@optimizely.com>
2015-10-25 17:00:18 -0700

    add setAMRImage as an extension to UIImageView

--- ArmoireApp/ArmoireApp/Models/AMRImage.swift
+++ ArmoireApp/ArmoireApp/Models/AMRImage.swift
@@ -10,33 +10,12 @@ import Foundation
 
 class AMRImage: PFObject {
     
-    @NSManaged var defaultImageName: String
-    @NSManaged private var file: PFFile
-    @NSManaged var client: AMRUser
-    @NSManaged var stylist: AMRUser
+    @NSManaged var defaultImageName: String?
+    @NSManaged private var file: PFFile?
+    @NSManaged var client: AMRUser?
+    @NSManaged var stylist: AMRUser?
     
-    func getImageView() -> UIImageView {
-        let imageView = PFImageView(image: UIImage(named: defaultImageName))
-        imageView.file = self.file
-        imageView.loadInBackground()
-        return imageView
-    }
-    
-    func getImage(completion: (image: UIImage?, error: NSError? ) -> ()) -> UIImage {
-        let image = UIImage(named:defaultImageName)
-        file.getDataInBackgroundWithBlock { (data: NSData?, error: NSError?) -> Void in
-            if error == nil {
-                let updatedImage = UIImage(data: data!)
-                completion(image: updatedImage, error: nil)
-            } else {
-                completion(image: nil, error: error)
-            }
-        }
-        return image!
-    }
-    
-    
-    class func imagesForUser(stylist: AMRUser?, client: AMRUser?, completion: (objects: [AMRNote]?, error: NSError?) -> Void)  {
+    class func imagesForUser(stylist: AMRUser?, client: AMRUser?, completion: (objects: [AMRImage]?, error: NSError?) -> Void)  {
         
         let query = self.query()
         if let stylist = stylist {
@@ -46,7 +25,7 @@ class AMRImage: PFObject {
             query?.whereKey("client", equalTo: client)
         }
         query?.findObjectsInBackgroundWithBlock({ (objects: [PFObject]?, error: NSError?) -> Void in
-            completion(objects: objects as? [AMRNote], error: error)
+            completion(objects: objects as? [AMRImage], error: error)
         })
     }
     
@@ -55,5 +34,22 @@ class AMRImage: PFObject {
 extension AMRImage: PFSubclassing {
     static func parseClassName() -> String {
         return "Image"
+    }
+}
+
+extension UIImageView {
+    func setAMRImage(image: AMRImage) {
+        if (image.defaultImageName != nil){
+            self.image = UIImage(named: image.defaultImageName!)
+        }
+        if (image.file != nil) {
+            image.file!.getDataInBackgroundWithBlock { (data: NSData?, error: NSError?) -> Void in
+                if error == nil {
+                    self.image = UIImage(data: data!)
+                } else {
+                    print("error loading image from file")
+                }
+            }
+        }
     }
 }
