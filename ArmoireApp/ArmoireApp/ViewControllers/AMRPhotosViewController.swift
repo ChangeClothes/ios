0866300a3537d6416b3b5fcf7885609c70ff0be5
Mathew Kellogg <mathew.kellogg@optimizely.com>
2015-10-29 22:06:01 -0700

    intermediate commit

--- ArmoireApp/ArmoireApp/ViewControllers/AMRPhotosViewController.swift
+++ ArmoireApp/ArmoireApp/ViewControllers/AMRPhotosViewController.swift
@@ -8,14 +8,51 @@
 
 import UIKit
 
-class AMRPhotosViewController: UIViewController, AMRViewControllerProtocol {
+let imageCellReuseIdentifier = "cell"
 
+class AMRPhotosViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource, AMRViewControllerProtocol{
+
+  
+  @IBOutlet weak var collectionView: UICollectionView!
   var stylist: AMRUser?
   var client: AMRUser?
+  var photos: [AMRImage] = []
+  
+  func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+      return photos.count
+  }
+  
+  func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    
+    let cell = collectionView.dequeueReusableCellWithReuseIdentifier(imageCellReuseIdentifier, forIndexPath: indexPath) as UICollectionViewCell
+    let image = photos[indexPath.row]
+    let imageView = UIImageView()
+    imageView.setAMRImage(image)
+    cell.backgroundView = imageView
+    return cell
+  }
+  
   
   override func viewDidLoad() {
-    super.viewDidLoad()
-    self.navigationController?.setNavigationBarHidden(true, animated: false)
+    AMRImage.imagesForUser(stylist, client: client) { (objects, error) -> Void in
+      self.photos = objects! as [AMRImage]
+      self.collectionView.reloadData()
+    }
+    //super.viewDidLoad()
+    //let cellNib = UINib(nibName: "AMRImageCell", bundle: nil)
+    //collectionView.registerNib(cellNib, forCellWithReuseIdentifier: imageCellReuseIdentifier)
+    //self.navigationController?.setNavigationBarHidden(true, animated: false)
+    
+    // Do any additional setup after loading the view, typically from a nib.
+    let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+    layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+    layout.itemSize = CGSize(width: 90, height: 120)
+    
+    collectionView.dataSource = self
+    collectionView.delegate = self
+    collectionView.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+    collectionView.backgroundColor = UIColor.whiteColor()
+    self.view.addSubview(collectionView)
     
     // Do any additional setup after loading the view.
   }
---
+++ ArmoireApp/ArmoireApp/Views/imageCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  imageCollectionViewCell.swift
+//  ArmoireApp
+//
+//  Created by Mathew Kellogg on 10/28/15.
+//  Copyright © 2015 Armoire. All rights reserved.
+//
+
+import UIKit
+
+class imageCollectionViewCell: UICollectionViewCell {
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+  }
+
+
+}
