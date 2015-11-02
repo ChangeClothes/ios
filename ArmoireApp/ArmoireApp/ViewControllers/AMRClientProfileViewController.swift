87b4c8a039aa294f1f233de4dba89adf185e8104
Randy Ting <randy@randyting.com>
2015-11-02 03:06:52 -0800

    Changed client profile to use segmented control and UIPageViewController.

--- ArmoireApp/ArmoireApp/ViewControllers/AMRClientProfileViewController.swift
+++ ArmoireApp/ArmoireApp/ViewControllers/AMRClientProfileViewController.swift
@@ -9,43 +9,52 @@
 import UIKit
 
 class AMRClientProfileViewController: AMRViewController, UIAlertViewDelegate, AMRViewControllerProtocol{
-
-  @IBOutlet weak var nameLabel: UILabel!
+  
+  // MARK: - IBOutlets
   @IBOutlet weak var containerView: UIView!
-  @IBOutlet weak var measurementImageView: UIImageView!
-  @IBOutlet weak var cameraImageView: UIImageView!
+  @IBOutlet weak var segmentedControl: UISegmentedControl!
   
-  @IBOutlet var vcTapGestureRecognizerOutlet: UITapGestureRecognizer!
-  /*******************************
-   *** AMRViewController LOGIC ***
-   ******************************/
+  // MARK: - Class Properties
   var selectedViewController: UIViewController?
-  var vcArray: [UINavigationController]!
-
-  //actions
-  @IBAction func cameraTap(sender: UITapGestureRecognizer) {
-    vcTapGestureRecognizerOutlet.enabled = false
-    selectViewController(vcArray[1])
-    
+  var vcArray: [UIViewController]!
+  var pageController: UIPageViewController!
+  
+  // MARK: - IBActions
+  @IBAction func segmentedControlDidChange(sender: UISegmentedControl) {
+    switch sender.selectedSegmentIndex {
+    case 0:
+      let initialViewControllerArray = [vcArray[0]]
+      pageController.setViewControllers(initialViewControllerArray, direction: .Reverse, animated: true, completion: nil)
+    case 1:
+      let initialViewControllerArray = [vcArray[1]]
+      pageController.setViewControllers(initialViewControllerArray, direction: .Forward, animated: true, completion: nil)
+    default:
+      break
+    }
   }
   
-  @IBAction func measurementTap(sender: UITapGestureRecognizer) {
-    vcTapGestureRecognizerOutlet.enabled = true
-    selectViewController(vcArray[0])
-  }
-  
-  @IBAction func vcTapGestureRecognizer(sender: UITapGestureRecognizer) {
-    self.view.endEditing(true)
-  }
-  
+  // MARK: - Lifecycle
   override func viewDidLoad() {
     self.navigationController?.navigationBar.translucent = false
     super.viewDidLoad()
+    
     setVcArray()
     setVcDataForTabs()
-    selectViewController(vcArray[0])
-    loadProfile()
+    
+    pageController = UIPageViewController(transitionStyle: .Scroll, navigationOrientation: .Horizontal, options: nil)
+    pageController.dataSource = self
+    pageController.delegate = self
+    pageController.view.frame = containerView.bounds
+    
+    let initialViewControllerArray = [vcArray[0]]
+    pageController.setViewControllers(initialViewControllerArray, direction: .Forward, animated: true, completion: nil)
+    addChildViewController(pageController)
+    containerView.addSubview(pageController.view)
+    pageController.didMoveToParentViewController(self)
+    
   }
+  
+  // MARK: - Initial Setup
   func setVcData(stylist: AMRUser?, client: AMRUser?) {
     self.stylist = stylist
     self.client = client
@@ -53,13 +62,27 @@ class AMRClientProfileViewController: AMRViewController, UIAlertViewDelegate, AM
       self.title = (client?.firstName)! + " " + (client?.lastName)!
     }
     setUpNavBar()
-
   }
-
-  func onSettingsTap(){
-    showSettings()
+  
+  private func setVcDataForTabs(){
+    for (index, value) in vcArray.enumerate() {
+      let vc = (value as! UINavigationController).viewControllers.first as? AMRViewControllerProtocol
+      vc?.setVcData(self.stylist, client: self.client)
+    }
   }
-
+  
+  private func setVcArray(){
+    let photoVC = AMRPhotosViewController()
+    let measurementsVC = AMRMeasurementsViewController()
+    
+    //    measurement.View.addGestureRecognizer(dismissKeyboardGR)
+    vcArray = [UINavigationController(rootViewController: photoVC), UINavigationController(rootViewController: measurementsVC)]
+  }
+  
+  func dismissKeyboard(sender: UITapGestureRecognizer) {
+    self.view.endEditing(true)
+  }
+  
   internal func setUpNavBar(){
     if (stylist != nil && client != nil){
       let leftNavBarButton = UIBarButtonItem(image: UIImage(named: "cancel"), style: .Plain, target: self, action: "exitModal")
@@ -69,46 +92,53 @@ class AMRClientProfileViewController: AMRViewController, UIAlertViewDelegate, AM
       self.navigationItem.leftBarButtonItem = leftNavBarButton
     }
   }
-
+  
+  // MARK: - Bar Button Actions
+  func onSettingsTap(){
+    showSettings()
+  }
+  
   func exitModal(){
     self.dismissViewControllerAnimated(true, completion: nil)
   }
   
-  private func setVcDataForTabs(){
-    for (index, value) in vcArray.enumerate() {
-      let vc = value.viewControllers.first as? AMRViewControllerProtocol
-      vc?.setVcData(self.stylist, client: self.client)
-    }
-  }
-  
-  private func setVcArray(){
-    let photoVC = UINavigationController(rootViewController: AMRPhotosViewController())
-    let measurementsVC = UINavigationController(rootViewController: AMRMeasurementsViewController())
-    vcArray = [measurementsVC, photoVC]
-  }
-  
-  func selectViewController(viewController: UIViewController){
-    if let oldViewController = selectedViewController{
-      oldViewController.willMoveToParentViewController(nil)
-      oldViewController.view.removeFromSuperview()
-      oldViewController.removeFromParentViewController()
+}
+
+extension AMRClientProfileViewController: UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+  func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+    if viewController.isEqual(vcArray[0]) {
+      return nil
     }
     
-    self.addChildViewController(viewController)
-    viewController.view.frame = self.containerView.bounds
-    viewController.view.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
-    self.containerView.addSubview(viewController.view)
-    viewController.didMoveToParentViewController(self)
-    selectedViewController = viewController
+    return vcArray[0]
   }
   
-  func loadProfile(){
-    nameLabel.text = client?.fullName ?? ""
+  func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+    if viewController.isEqual(vcArray[1]) {
+      return nil
+    }
+    
+    return vcArray[1]
   }
   
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  func pageViewController(pageViewController: UIPageViewController, willTransitionToViewControllers pendingViewControllers: [UIViewController]) {
+    if pendingViewControllers[0].isEqual(vcArray[0]){
+      segmentedControl.selectedSegmentIndex = 0
+    } else {
+      segmentedControl.selectedSegmentIndex = 1
+    }
+  }
+  
+  func pageViewController(pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+    if previousViewControllers[0].isEqual(vcArray[0]) {
+      print(((vcArray[1] as! UINavigationController).viewControllers.first as! AMRMeasurementsViewController).view.gestureRecognizers?.count)
+      
+      if ((vcArray[1] as! UINavigationController).viewControllers.first as! AMRMeasurementsViewController).view.gestureRecognizers == nil {
+        let dismissKeyboardGR = UITapGestureRecognizer(target: self, action: "dismissKeyboard:")
+        (vcArray[1] as! UINavigationController).viewControllers.first?.view.addGestureRecognizer(dismissKeyboardGR)
+      }
+    }
+    
   }
   
 }
