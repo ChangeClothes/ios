4f84040bf28e7e0fed9fb5f9239474ee64e2a09b
Randy Ting <randy@randyting.com>
2015-11-07 17:07:30 -0800

    Removed ParseUI cocoapod.

--- ArmoireApp/ArmoireApp/Application/ArmoireApp-Bridging-Header.h
+++ ArmoireApp/ArmoireApp/Application/ArmoireApp-Bridging-Header.h
@@ -2,7 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import <ParseUI/ParseUI.h>
 #import <Parse/Parse.h>
 #import <Parse/PFObject+Subclass.h>
 #import <Atlas/Atlas.h>
