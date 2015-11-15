4fb445b663644803ef3d589f885ae22eada40ce0
Randy Ting <randy@randyting.com>
2015-11-15 00:16:21 -0800

    Removed fetching for client to set avatar image.

--- ArmoireApp/ArmoireApp/Views/AMRTodayTableViewCell.swift
+++ ArmoireApp/ArmoireApp/Views/AMRTodayTableViewCell.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class AMRTodayTableViewCell: UITableViewCell {
-
+  
   @IBOutlet weak var avatarImage: UIImageView!
   
   @IBOutlet weak var nameLabel: UILabel!
@@ -19,15 +19,48 @@ class AMRTodayTableViewCell: UITableViewCell {
   
   @IBOutlet weak var unratedPhotosHeightConstraint: NSLayoutConstraint!
   
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+  var client: AMRUser! {
+    didSet {
+      let badges = AMRBadgeManager.sharedInstance.clientBadges[client]
+      
+      self.avatarImage.layer.cornerRadius = self.avatarImage.frame.width/2
+      self.avatarImage.clipsToBounds = true
+      self.avatarImage.setAMRImage(client.profilePhoto, withPlaceholder: "profile-image-placeholder")
+      
 
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+      nameLabel.text = client.firstName + " " + client.lastName
+      appointmentsLabelHeightConstraint.constant = 0
+      newMessagesLabelHeightConstraint.constant = 0
+      unratedPhotosHeightConstraint.constant = 0
+      
+      if badges?.hasMeetingToday == true {
+        appointmentsLabelHeightConstraint.constant = 16
+      }
+      if badges?.hasUnreadMessages == true {
+        newMessagesLabelHeightConstraint.constant = 16
+      }
+      if badges?.hasUnratedPhotos == true {
+        unratedPhotosHeightConstraint.constant = 16
+      }
+      
+      
     }
+  }
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+  }
+  
+  override func setSelected(selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
     
+    // Configure the view for the selected state
+  }
+  
+  override func prepareForReuse() {
+    avatarImage.image = nil
+  }
+  
+  
 }
