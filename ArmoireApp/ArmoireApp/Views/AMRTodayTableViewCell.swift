8b37ee16085313105948f37898f1c0b34bb8e3b3
Randy Ting <randy@randyting.com>
2015-11-15 17:42:33 -0800

    Basic implementation of today cell  ui.

--- ArmoireApp/ArmoireApp/Views/AMRTodayTableViewCell.swift
+++ ArmoireApp/ArmoireApp/Views/AMRTodayTableViewCell.swift
@@ -13,9 +13,9 @@ class AMRTodayTableViewCell: UITableViewCell {
   let labelHeight = CGFloat(16)
   let iconHeight = CGFloat(12)
   
+  @IBOutlet weak var appointmentIconHeightConstraint: NSLayoutConstraint!
   @IBOutlet weak var cellBackgroundView: UIView!
   @IBOutlet weak var appointmentIconView: UIImageView!
-  @IBOutlet weak var appointmentIconHeightConstraint: NSLayoutConstraint!
   @IBOutlet weak var newMessageIconView: UIImageView!
   @IBOutlet weak var newMessageIconHeightConstraint: NSLayoutConstraint!
   @IBOutlet weak var unratedPhotoIconView: UIImageView!
@@ -47,11 +47,13 @@ class AMRTodayTableViewCell: UITableViewCell {
       unratedPhotosHeightConstraint.constant = 0
       unratedPhotoIconHeightConstraint.constant = 0
       
-      appointmentIconView.tintColor = ThemeManager.currentTheme().highlightColor
-      newMessageIconView.tintColor = ThemeManager.currentTheme().highlightColor
-      unratedPhotoIconView.tintColor = ThemeManager.currentTheme().highlightColor
+      appointmentIconView.tintColor = UIColor.darkGrayColor()
+      newMessageIconView.tintColor = UIColor.darkGrayColor()
+      unratedPhotoIconView.tintColor = UIColor.darkGrayColor()
       
-      
+      appointmentIconView.image = appointmentIconView.image?.imageWithRenderingMode(.AlwaysTemplate)
+      newMessageIconView.image = newMessageIconView.image?.imageWithRenderingMode(.AlwaysTemplate)
+      unratedPhotoIconView.image = unratedPhotoIconView.image?.imageWithRenderingMode(.AlwaysTemplate)
       
       
       if badges?.hasMeetingToday == true {
@@ -61,6 +63,7 @@ class AMRTodayTableViewCell: UITableViewCell {
       if badges?.hasUnreadMessages == true {
         newMessagesLabelHeightConstraint.constant = labelHeight
         newMessageIconHeightConstraint.constant = iconHeight
+        newMessageIconView.tintColor = ThemeManager.currentTheme().highlightColor
       }
       if badges?.hasUnratedPhotos == true {
         unratedPhotosHeightConstraint.constant = labelHeight
@@ -70,21 +73,33 @@ class AMRTodayTableViewCell: UITableViewCell {
       
     }
   }
+
+  func didTapCell(sender: UITapGestureRecognizer) {
+    switch sender.state {
+    case .Began:
+      cellBackgroundView.backgroundColor = ThemeManager.currentTheme().highlightColor
+    case .Ended:
+      cellBackgroundView.backgroundColor = UIColor.clearColor()
+    default:
+      break
+    }
+    
+  }
   
   override func awakeFromNib() {
     super.awakeFromNib()
-    cellBackgroundView.backgroundColor = ThemeManager.currentTheme().todayViewCellBackgroundColor
+//    self.selectionStyle = .None
+    
     cellBackgroundView.layer.cornerRadius = 8.0
     cellBackgroundView.clipsToBounds = true
-    cellBackgroundView.layer.borderColor = ThemeManager.currentTheme().highlightColor.CGColor
-    cellBackgroundView.layer.borderWidth = 1.0
-    // Initialization code
+    cellBackgroundView.layer.borderColor = UIColor.blackColor().CGColor
+    cellBackgroundView.layer.borderWidth = 2.0
+    
   }
   
   override func setSelected(selected: Bool, animated: Bool) {
     super.setSelected(selected, animated: animated)
     
-    // Configure the view for the selected state
   }
   
   override func prepareForReuse() {
